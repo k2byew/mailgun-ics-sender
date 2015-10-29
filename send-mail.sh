84cd642759cc1fa6d3d5c9c8abf7078cda2ce2fa
k2byew <k2byew@users.noreply.github.com>
2015-10-29 23:22:03 +1300

    Create send-mail.sh

---
+++ send-mail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+email_to="name <email@email.com>"
+email_subject="email subject"
+email_attachment=filename.ics
+api_key=key-1234567890abcdefghijklmnopqrstuv
+api_base_url=https://api.mailgun.net/v3/sandboxcc1234567890abcdefghijklmnopqrst.mailgun.org
+
+curl -v -s --user "api:$api_key" \
+    "$api_base_url"/messages \
+    -F from='Name Here <no-reply@something.com>' \
+    -F to="$email_to" \
+    -F subject="$email_subject" \
+    -F text='Email body text here' \
+    -F attachment=@"$email_attachment"
